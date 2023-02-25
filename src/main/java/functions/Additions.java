ea4d53236ca25d22c817e8b16fc1f6d5b333cb2c
Hasancan Özen <43709185+hasancanozen@users.noreply.github.com>
2023-02-25 20:52:50 +0300

    first test class wrote

---
+++ src/main/java/functions/Additions.java
@@ -0,0 +1,11 @@
+package functions;
+
+public class Additions {
+
+    public int additionInteger(int x, int y){
+        return x + y;
+    }
+    public double additionDouble(double x, double y){
+        return x + y;
+    }
+}
